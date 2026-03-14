52398694079ec46e89f752df2d4d3554a9e6d7e4
StephenNgo97 <stephenngo97@gmail.com>
2026-03-15 10:58:11 +1100

    Added asteroid collision/splitting logic with shots

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,9 @@
 
+import random
+from logger import log_event
 from circleshape import CircleShape
 import pygame
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
 
@@ -13,3 +15,17 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
+    
+    def split(self):
+        self.kill()
+        if self.radius < ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        spawn_angle = random.uniform(20, 50)
+        child_vector1 = self.velocity.rotate(spawn_angle)
+        child_vector2 = self.velocity.rotate(-spawn_angle)
+        child_radius = self.radius - ASTEROID_MIN_RADIUS
+        child_asteroid1 = Asteroid(self.position.x, self.position.y, child_radius)
+        child_asteroid1.velocity = child_vector1 * 1.2
+        child_asteroid2 = Asteroid(self.position.x, self.position.y, child_radius)
+        child_asteroid2.velocity = child_vector2 * 1.2
--- main.py
+++ main.py
@@ -42,6 +42,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
         for object in drawable:
             object.draw(screen)
         pygame.display.flip()
