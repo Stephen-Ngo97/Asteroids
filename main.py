0d110348cd5f37375131e55bd1295fe987b783a9
StephenNgo97 <stephenngo97@gmail.com>
2026-03-14 21:26:31 +1100

    Created player class and rendered player on screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 import pygame
 from logger import log_state
+from player import Player
 
 def main():
     print("Starting Asteroids with pygame version:", pygame.version.ver)
@@ -11,12 +12,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
